b2be0ffcdad7e8b3550acf3d2214afde63f7de2a
Batu A. <batuakyar07@gmail.com>
2025-08-02 14:50:16 +0300

    build ins

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-# requirements.txt ile bağımlılıkları yükle
 pip install -r requirements.txt
-
-# Playwright için Chromium'u indir
-playwright install chromium
+npx playwright install --with-deps
